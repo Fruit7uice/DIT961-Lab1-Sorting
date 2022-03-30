f9ac7226ffb6acb06caaa9b1b63e745b70abffe8
Fruit7uice <36786942+Fruit7uice@users.noreply.github.com>
2022-03-30 14:27:20 +0200

    InsertionSort Implemented

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,14 +1,47 @@
+import java.util.Arrays;
+
 public class Lab1 {
     /** Sorting algorithms **/
 
     // Insertion sort.
+    public static void main(String[] args) {
+        //int[] a = {1,3,5,4,0};
+        //int[] a = {0, 1, 2, 3, 4, 5};
+        int[] a = {0,3,3,4,4,1};
 
-    public static void insertionSort(int[] array) {
-        throw new UnsupportedOperationException();
+        System.out.println(Arrays.toString(a));
+        insertionSort(a);
+        System.out.println(Arrays.toString(a));
     }
 
-    // Quicksort.
+    public static void insertionSort(int[] array) {
+        //boolean sorted = false;
+        int index = 1;
+        while (index < array.length){
+            int pivot = array[index];
 
+            if (pivot < array[index-1]){ // If the pivot is less than its predecessor
+
+                for (int i = index-1; i >= 0; i--) { // Loop backwards moving elements up one step to the right.(+1)
+                    if (pivot < array[i]){ // If the pivot is less than its predecessor
+                        array[i+1] = array[i]; // move the element up (+1)
+                    }
+                    else{ // if the predecessor is less than the pivot, place it.
+                        array[i+1] = pivot;
+                        break;
+                    }
+                    if (i == 0){ // if the pivot doesn't have a predecessor, place it first
+                        array[i] = pivot;
+                    }
+                }
+            }
+            index++;
+        }
+
+    }
+
+
+    // Quicksort.
     public static void quickSort(int[] array) {
         throw new UnsupportedOperationException();
     }
